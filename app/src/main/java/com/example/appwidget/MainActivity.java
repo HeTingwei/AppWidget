d2b0301ea0900a34281251a25b43ae335883ffd6
HeTingwei <2249382610@qq.com>
2017-11-04 16:00:25 +0800

    完善代码

--- app/src/main/java/com/example/appwidget/MainActivity.java
+++ app/src/main/java/com/example/appwidget/MainActivity.java
@@ -7,14 +7,31 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textview = (TextView) findViewById(R.id.textView);
-        Intent intent = getIntent();
-        int result=intent.getIntExtra("data",0);
-        textview.setText(result+"");
+        textview = (TextView) findViewById(R.id.textView);
+        changeText(getIntent());
     }
 
+    /*onNewIntent():
+    * This is called for activities that set launchMode to "singleTop" in their package, or if
+    * a client used the FLAG_ACTIVITY_SINGLE_TOP flag when calling startActivity(Intent).
+    * In either case, when the activity is re-launched while at the top of the activity stack
+    * instead of a new instance of the activity being started, onNewIntent() will be called on
+    * the existing instance with the Intent that was used to re-launch it.
+    * */
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        changeText(intent);
+    }
+
+    private void changeText(Intent intent){
+        int result=intent.getIntExtra("data",-1);
+        if(result!=-1)
+        textview.setText(result+"");
+    }
 }
