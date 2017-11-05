9f301396dd173e6a93e1843e8058bb410a155717
HeTingwei <2249382610@qq.com>
2017-11-05 13:17:03 +0800

    完善代码

--- app/src/main/java/com/example/appwidget/MyBroadcast.java
+++ app/src/main/java/com/example/appwidget/MyBroadcast.java
@@ -10,25 +10,20 @@ public class MyBroadcast extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		String str = intent.getAction();
 
-		if (str.endsWith("msg1")) {
-			Intent intent1 = new Intent(context, MainActivity.class);
-			intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			intent1.putExtra("data", 1);
-			context.startActivity(intent1);
-		}
+		Intent intent1 = new Intent(context, MainActivity.class);
+		intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		intent1.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
 
+		if (str.endsWith("msg1")) {
+			intent1.putExtra("data", 1);
+		}
 		if (str.endsWith("msg2")) {
-			Intent intent1 = new Intent(context, MainActivity.class);
-			intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 			intent1.putExtra("data", 2);
-			context.startActivity(intent1);
 		}
 		if (str.endsWith("msg3")) {
-			Intent intent1 = new Intent(context, MainActivity.class);
-			intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 			intent1.putExtra("data", 3);
-			context.startActivity(intent1);
 		}
+		context.startActivity(intent1);
 
 	}
 
